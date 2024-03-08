9fbacecd9dc803ee6c8e2bfe59d9400bf7bd0219
darenky <kuyesdaren@gmail.com>
2024-03-08 21:47:41 +0200

    add lines to run tests faster

--- lab1/test_time.py
+++ lab1/test_time.py
@@ -1,7 +1,17 @@
 import unittest
 import dumb_code
 
+#v l
+
 class TestTime(unittest.TestCase):
 
     def test_time(self):
+        self.assertEqual(dumb_code.time(3, 12), 4)
+        self.assertEqual(dumb_code.time(-3, 12), )
+        self.assertEqual(dumb_code.time(3, -12), )
+        self.assertEqual(dumb_code.time(-3, -12), )
         
+
+
+if __name__ == '__main__':
+    unittest.main()
