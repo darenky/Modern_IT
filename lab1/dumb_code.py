a524c76bfcaae9a04d81c0aa481c21feb5e9495b
darenky <kuyesdaren@gmail.com>
2024-03-08 22:11:15 +0200

    Lab done

--- lab1/dumb_code.py
+++ lab1/dumb_code.py
@@ -4,6 +4,9 @@ def time(speed, distance):
     if speed == 0:
         raise ValueError("Can't divide by 0")
     
+    if speed < 0 or distance < 0:
+        raise ValueError("In my task, speed or distance can't be negative.")
+    
     time = distance / speed
 
     return time
--- lab1/test_time.py
+++ lab1/test_time.py
@@ -1,17 +1,23 @@
 import unittest
 import dumb_code
 
-#v l
-
 class TestTime(unittest.TestCase):
 
     def test_time(self):
         self.assertEqual(dumb_code.time(3, 12), 4)
 
         with self.assertRaises(ValueError):
-            dumb_code.time(0,12)
+            dumb_code.time(0, 12)
         
+        with self.assertRaises(ValueError):
+            dumb_code.time(-1, 12)
 
+        with self.assertRaises(ValueError):
+            dumb_code.time(1, -12)
 
+        with self.assertRaises(ValueError):
+            dumb_code.time(-1, -12)
+
+        
 if __name__ == '__main__':
     unittest.main()
