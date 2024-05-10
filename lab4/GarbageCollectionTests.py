451670ce24e339a4d2ae2bdedce2b97c738ffcfd
darenky <kuyesdaren@gmail.com>
2024-05-10 17:23:37 +0300

    Modify tests for GC

--- lab4/GarbageCollectionTests.py
+++ lab4/GarbageCollectionTests.py
@@ -1,46 +1,38 @@
 import unittest
 import gc
+import sys  
 
-class MyObject:
-    def __init__(self, name):
-        self.name = name
-        self.obj_info = f"Object Name: {name}, Object ID: {id(self)}"
+class TestGarbageCollector(unittest.TestCase):
 
-    def __del__(self):
-        print(f"Object with ID: {id(self)} is being deleted.")
+    def setUp(self):
+        self.large_object = [None] * (10 ** 7)  
 
-def manual_collect():
-    print("Calling garbage collector...")
-    collected = gc.collect()
-    print(f"Unreachable objects collected: {collected}")
-    print("Garbage collection finished.")
+    def tearDown(self):
+        del self.large_object
+        gc.collect()
 
-class TestGarbageCollection(unittest.TestCase):
-    def test_garbage_collection(self):
-        obj1 = MyObject("Object 1")
-        obj2 = MyObject("Object 2")
-        obj3 = MyObject("Object 3")
+    def test_collection(self):
+        large_object = [None] * (10 ** 7)
+        del large_object
+        gc.collect()
+        self.assertLess(gc.get_count()[0], 10)
 
-        self.assertEqual(obj1.obj_info, "Object Name: Object 1, Object ID: {}".format(id(obj1)))
-        self.assertEqual(obj2.obj_info, "Object Name: Object 2, Object ID: {}".format(id(obj2)))
-        self.assertEqual(obj3.obj_info, "Object Name: Object 3, Object ID: {}".format(id(obj3)))
+    def test_reference_counting(self):
+        obj = object()
+        initial_refcount = sys.getrefcount(obj)
+        another_ref = obj
+        self.assertEqual(sys.getrefcount(obj), initial_refcount + 1)
+        del another_ref
+        self.assertEqual(sys.getrefcount(obj), initial_refcount)
 
-        del obj1
-        del obj2
+    def test_cycle_detection(self):
+        obj1 = {}
+        obj2 = {}
+        obj1['obj2'] = obj2
+        obj2['obj1'] = obj1
+        del obj1, obj2
+        gc.collect()
+        self.assertEqual(gc.get_count()[0], 0)
 
-        manual_collect()
-
-        obj4 = MyObject("Object 4")
-        obj5 = MyObject("Object 5")
-        obj4.obj5 = obj5
-        obj5.obj4 = obj4
-
-        manual_collect()
-
-        del obj4
-        del obj5
-
-        manual_collect()
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
