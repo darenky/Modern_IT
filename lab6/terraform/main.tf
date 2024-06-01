6859e73e17272c5339f9daafe473c66d60ecd87b
darenky <kuyesdaren@gmail.com>
2024-06-01 10:15:55 +0300

    add lab6

---
+++ lab6/terraform/main.tf
@@ -0,0 +1,95 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "eu-central-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    lambda = "http://localhost:4566"
+    iam    = "http://localhost:4566"
+    s3     = "http://s3.localhost.localstack.cloud:4566"
+  }
+}
+
+# Define the IAM role for the Lambda function
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda-exec-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
+
+# Create the Lambda function
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda/"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_lambda_function" "file_transfer" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = "file-transfer-lambda"
+  handler          = "main.lambda_handler"
+  runtime          = "python3.8"
+  role             = aws_iam_role.lambda_role.arn
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+# Define the S3 bucket lifecycle and event notification
+resource "aws_s3_bucket_lifecycle_configuration" "source_bucket_lifecycle" {
+  bucket = aws_s3_bucket.source_bucket.id
+
+  rule {
+    id     = "delete-objects-after-7-days"
+    status = "Enabled"
+
+    expiration {
+      days = 7
+    }
+  }
+}
+
+# Create the S3 buckets
+resource "aws_s3_bucket" "source_bucket" {
+  bucket = "source-bucket"
+}
+
+resource "aws_s3_bucket" "destination_bucket" {
+  bucket = "destination-bucket"
+}
+
+resource "aws_lambda_permission" "allow_bucket_invoke" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.file_transfer.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.source_bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "source_bucket_notification" {
+  bucket = aws_s3_bucket.source_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.file_transfer.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [
+    aws_lambda_permission.allow_bucket_invoke
+  ]
+}
